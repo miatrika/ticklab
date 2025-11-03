457917b3b5862554434e135b15abfab5a5384bb7
miatrika <miatrikatendriariniavo1@gmail.com>
2025-11-03 11:54:37 +0300

    creation dossier test units dans test.groovy

--- jenkins/stages/test.groovy
+++ jenkins/stages/test.groovy
@@ -9,6 +9,8 @@ try {
     # Lance migrations + tests dans un conteneur temporaire
     docker-compose run --rm -T -e CI=true -e APP_ENV=testing app sh -c '
         cp .env.testing .env &&
+        mkdir -p storage/framework/cache/data storage/framework/views storage/framework/sessions storage/logs &&
+        chmod -R 777 storage &&
         php artisan migrate:fresh --seed --force &&
         vendor/bin/phpunit --configuration phpunit.xml
     '
