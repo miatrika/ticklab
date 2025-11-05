84820e7b377d74c73f17d3a6ad397a9a59da4d1a
miatrika <miatrikatendriariniavo1@gmail.com>
2025-11-05 15:10:08 +0300

    finition deploy.groovy

--- jenkins/stages/deploy.groovy
+++ jenkins/stages/deploy.groovy
@@ -1,21 +1,26 @@
 echo "=== STAGE: Deploy to remote server ==="
 
-sshagent(['deploy-ssh']){
+sshagent(['deploy-ssh']) {
     sh """
+      # Crée les dossiers sur le serveur
       ssh -o StrictHostKeyChecking=no ${env.DEPLOY_USER}@${env.DEPLOY_HOST} '
-         mkdir -p ${env.DEPLOY_PATH}
+         mkdir -p ${env.DEPLOY_PATH}/nginx
       '
-      scp -o StrictHostKeyChecking=no docker-compose.prod.yml ${env.DEPLOY_USER}@${env.DEPLOY_HOST}:${env.DEPLOY_PATH}/docker-compose.yml
 
-       ssh -o StrictHostKeyChecking=no ${env.DEPLOY_USER}@${env.DEPLOY_HOST} '
+      # Copie docker-compose et default.conf
+      scp -o StrictHostKeyChecking=no docker-compose.prod.yml ${env.DEPLOY_USER}@${env.DEPLOY_HOST}:${env.DEPLOY_PATH}/docker-compose.yml
+      scp -o StrictHostKeyChecking=no nginx/default.conf ${env.DEPLOY_USER}@${env.DEPLOY_HOST}:${env.DEPLOY_PATH}/nginx/default.conf
+
+      # Lancer les conteneurs
+      ssh -o StrictHostKeyChecking=no ${env.DEPLOY_USER}@${env.DEPLOY_HOST} '
          set -eux
          cd ${env.DEPLOY_PATH}
-        IMAGE_TAG=${env.BUILD_NUMBER} docker-compose pull
-        IMAGE_TAG=${env.BUILD_NUMBER} docker-compose up -d
-       '
-      """
-  }
-echo
+         IMAGE_TAG=${env.BUILD_NUMBER} docker-compose pull
+         IMAGE_TAG=${env.BUILD_NUMBER} docker-compose up -d --remove-orphans
+      '
+    """
+}
+echo""
 sh """
 ssh -o StrictHostKeyChecking=no ${env.DEPLOY_USER}@${env.DEPLOY_HOST} "curl -fs http://localhost:${env.HOST_HTTP_PORT ?: 80}"
 """
