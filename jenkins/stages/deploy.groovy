a2145d13dcc31fc50402ec6ef809fcd8fe649353
miatrika <miatrikatendriariniavo1@gmail.com>
2025-11-05 11:14:42 +0300

    optimisation de deploiement sur machine distante

--- jenkins/stages/deploy.groovy
+++ jenkins/stages/deploy.groovy
@@ -2,11 +2,20 @@ echo "=== STAGE: Deploy to remote server ==="
 
 sshagent(['deploy-ssh']) {
     sh """
+    # Créer un dossier temporaire sur le serveur pour le déploiement
+    ssh -o StrictHostKeyChecking=no ${env.DEPLOY_USER}@${env.DEPLOY_HOST} '
+      mkdir -p ${env.DEPLOY_PATH}
+    '
+
+    # Copier le docker-compose.yml depuis Jenkins vers le serveur
+    scp -o StrictHostKeyChecking=no docker-compose.yml ${env.DEPLOY_USER}@${env.DEPLOY_HOST}:${env.DEPLOY_PATH}/docker-compose.yml
+
+    # Lancer docker-compose sur le serveur pour pull les images et démarrer les conteneurs
     ssh -o StrictHostKeyChecking=no ${env.DEPLOY_USER}@${env.DEPLOY_HOST} '
       set -eux
       cd ${env.DEPLOY_PATH}
-      docker-compose pull
-      docker-compose up -d --build
+      docker-compose pull       # récupérer les images déjà construites
+      docker-compose up -d      # démarrer les conteneurs
     '
     """
 }
