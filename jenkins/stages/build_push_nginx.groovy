76bddf2ee44a1307084c5c48e25855c8935896af
miatrika <miatrikatendriariniavo1@gmail.com>
2025-11-14 15:57:25 +0300

    docker-compose -> docker compose 6

--- jenkins/stages/build_push_nginx.groovy
+++ jenkins/stages/build_push_nginx.groovy
@@ -1,15 +1,25 @@
-echo "=== STAGE: Build & Push Nginx image ==="
+echo "=== 🚀 STAGE: Build & Push Nginx image ==="
 
-sh """
-set -eux
-export DOCKER_BUILDKIT=1
-docker build -t docker.io/miatrika05/ticklab-nginx:${IMAGE_TAG} \
---build-arg APP_IMAGE=docker.io/miatrika05/ticklab:${IMAGE_TAG} \
--f docker/nginx/Dockerfile .
 withCredentials([usernamePassword(credentialsId: 'dockerhub-creds', usernameVariable: 'DOCKERHUB_USER', passwordVariable: 'DOCKERHUB_PASS')]) {
-    echo \$DOCKERHUB_PASS | docker login -u \$DOCKERHUB_USER --password-stdin
+    sh '''
+    #!/bin/bash
+    set -eux
+    export DOCKER_BUILDKIT=1
+
+    # Build Nginx image
+    docker build -t docker.io/miatrika05/ticklab-nginx:${IMAGE_TAG} \
+        --build-arg APP_IMAGE=docker.io/miatrika05/ticklab:${IMAGE_TAG} \
+        -f docker/nginx/Dockerfile .
+
+    # Login to Docker Hub
+    echo "$DOCKERHUB_PASS" | docker login -u "$DOCKERHUB_USER" --password-stdin
+
+    # Push Nginx image
     docker push docker.io/miatrika05/ticklab-nginx:${IMAGE_TAG}
+
+    # Logout
     docker logout
+    '''
 }
-"""
+
 echo "✅ Nginx image pushed"
