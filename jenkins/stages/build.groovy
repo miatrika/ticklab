cd13c133862c72f1ee276586246c7e37a8be3800
miatrika <miatrikatendriariniavo1@gmail.com>
2025-11-14 14:31:10 +0300

    docker-compose -> docker compose

--- jenkins/stages/build.groovy
+++ jenkins/stages/build.groovy
@@ -2,7 +2,7 @@ echo "=== STAGE: Build containers & composer install (cached) ==="
 
 sh '''
 set -eux
-docker-compose build app
-docker-compose run --rm -e CI=true app php -v
+docker compose build app
+docker compose run --rm -e CI=true app php -v
 echo "✅ Build stage completed successfully."
 '''
