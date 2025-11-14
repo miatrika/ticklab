b17c4134ebc47d4f95be6ce05b4cfa841fffb3ba
miatrika <miatrikatendriariniavo1@gmail.com>
2025-11-14 14:30:48 +0300

    docker-compose -> docker compose

--- jenkins/stages/qa.groovy
+++ jenkins/stages/qa.groovy
@@ -2,7 +2,7 @@ echo "=== STAGE: QA - PHPCS / PHPStan ==="
 
 sh '''
 set -eux
-docker-compose run --rm app vendor/bin/phpcs --standard=PSR12 --extensions=php app || true
-docker-compose run --rm app vendor/bin/phpstan analyse --configuration=phpstan.neon || true
+docker compose run --rm app vendor/bin/phpcs --standard=PSR12 --extensions=php app || true
+docker compose run --rm app vendor/bin/phpstan analyse --configuration=phpstan.neon || true
 '''
 echo "=== QA stage finished ==="
--- jenkins/stages/test.groovy
+++ jenkins/stages/test.groovy
@@ -3,8 +3,8 @@ echo "=== STAGE: Run Laravel tests (phpunit) ==="
 try {
     sh '''
     set -eux
-    docker-compose up -d db
-    docker-compose run --rm -T -e CI=true -e APP_ENV=testing app sh -c '
+    docker compose up -d db
+    docker compose run --rm -T -e CI=true -e APP_ENV=testing app sh -c '
         cp .env.testing .env &&
         mkdir -p storage/framework/cache/data storage/framework/views storage/framework/sessions storage/logs &&
         chmod -R 777 storage &&
