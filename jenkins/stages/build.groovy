067d334a40595cbd2aa9bdc121a55405378aaf5a
miatrika <miatrikatendriariniavo1@gmail.com>
2025-11-20 15:57:42 +0300

    essaie https sans avertissement 12

--- jenkins/stages/build.groovy
+++ jenkins/stages/build.groovy
@@ -5,7 +5,8 @@
         set -eux
 
         docker compose down -v || true
-
+        docker compose up -d db
+        
         # Build l’image de l’app en utilisant l’ancien docker-compose
         docker compose build app
         until docker compose exec -T db mysqladmin ping -h "127.0.0.1" --silent; do
