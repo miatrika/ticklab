e820aa896f66e02c5c81933f50e00ac4f194f6dd
miatrika <miatrikatendriariniavo1@gmail.com>
2025-11-20 15:51:27 +0300

    essaie https sans avertissement 11

--- jenkins/stages/build.groovy
+++ jenkins/stages/build.groovy
@@ -5,9 +5,13 @@
         set -eux
 
         docker compose down -v || true
-        
+
         # Build l’image de l’app en utilisant l’ancien docker-compose
         docker compose build app
+        until docker compose exec -T db mysqladmin ping -h "127.0.0.1" --silent; do
+        echo "Waiting for MySQL..."
+        sleep 3
+        done
 
         # Vérifier PHP
         docker compose run --rm -e CI=true app php -v
