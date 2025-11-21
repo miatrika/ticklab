69eec8eff02d5bab7c34b9964d1de18dd0d55be3
miatrika <miatrikatendriariniavo1@gmail.com>
2025-11-21 14:45:12 +0300

    resolution php8.2 composer.lock

--- jenkins/stages/build.groovy
+++ jenkins/stages/build.groovy
@@ -5,11 +5,7 @@
         set -eux
         # Build l’image de l’app en utilisant l’ancien docker-compose
         docker compose build --no-cache app
-
-        docker compose run --rm app composer update --lock 
-        # Installer les dépendances (respecte maintenant le lock file mis à jour) 
-        docker compose run --rm -e CI=true app composer install --no-interaction --prefer-dist --optimize-autoloader --no-scripts
-
+        
         # Vérifier PHP
         docker compose run --rm -e CI=true app php -v
         '''
